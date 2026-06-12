583c08c03ff75747183774a254f23693451a154c
sachin diwakar <sachindiwakar1802@gmail.com>
2026-06-12 17:09:19 +0530

    code

---
+++ loops/for_loop.js
@@ -0,0 +1,13 @@
+//  // for
+
+//  for(let i=0;i<20;i++){
+//   console.log(i)
+//  }
+//  // for inside for
+//  for(let i=0;i<=10;i++){
+//   console.log(`outer loop value:${i}`);
+//   for(let j=0;j<=10;j++){
+//     console.log(`Inner loop value ${j} and inner loop ${i}`);
+//   }
+//  }
+// break and continue in the loops in the for lops to use 
