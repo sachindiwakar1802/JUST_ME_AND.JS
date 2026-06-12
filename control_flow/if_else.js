ac845e53a760d15dbaa92e3072010924950601df
sachin diwakar <sachindiwakar1802@gmail.com>
2026-06-12 16:22:28 +0530

    code

---
+++ control_flow/if_else.js
@@ -0,0 +1,22 @@
+// if
+
+const isUserloggedIn = true;
+if(isUserloggedIn){
+
+}
+else{
+  
+}
+
+// <, >, <=,==,===,!=(2!=3) => correct!
+/*
+we use the if else for teh control flow which code run at the spcific match or not so can contaol the flow of the code
+*/
+
+// for both the conditions are true 
+const userLoggedIn = true;
+const debitCard = true;
+const loggggoogle = false;
+if(userLoggedIn && debitCard || loggggoogle){
+console.log("Allow to buy course ");
+}
---
+++ control_flow/switch.js
@@ -0,0 +1,24 @@
+// switch(key){
+//   case value:3;
+//   break;
+//   case value:4;
+//   break;
+//   case value:5;
+//   break;
+//   case value:6;
+//   break;
+// }
+
+// month 
+// const month = 0;
+
+// switch (month){
+//   case 1: console.log("january");
+//   break;
+//   case 2: console.log("fabrury");
+//   break;
+//   case 3: console.log("march");
+//   default:
+//     console.log("choose right month number")
+//     break;
+// } 
