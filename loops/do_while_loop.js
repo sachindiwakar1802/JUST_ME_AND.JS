437e2177ed19ca7f832df4f61d4c2212fea37be1
sachin diwakar <sachindiwakar1802@gmail.com>
2026-06-12 17:29:35 +0530

    code

---
+++ loops/do_while_loop.js
@@ -0,0 +1,7 @@
+// do while loop => pehle kaam akro fir condition chek akro 
+let i= 0 
+do{
+ console.log(`the value is${i}`)
+ i++
+ }
+ while( i<=10)
---
+++ loops/while_loop.js
@@ -0,0 +1,6 @@
+
+let i =0;
+while(i<=10){
+  console.log(`value of index is ${index}`);
+  i = i+3
+}
