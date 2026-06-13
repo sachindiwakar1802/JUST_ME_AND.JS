0a12fc000969a1ed373cdaa1d9e34b01069d054e
sachin diwakar <sachindiwakar1802@gmail.com>
2026-06-13 17:55:27 +0530

    code

---
+++ loops/for_of_loop.js
@@ -0,0 +1,14 @@
+// for_of  these are array orientd 
+
+// ["" ,"" , ""];
+// [{},{},{}];
+
+// const arr = [1,2,3,4,5];
+// for(const num of arr){
+//   console.log(num)
+// }
+
+let greet = "greeting"
+for(const i of greet){
+  console.log(i)
+}
